3646bae1f04f853682f5252f5f74e1ff0f1bb15c
Jan <github@geheimwerk.de>
2012-11-08 16:45:52 +0100

    Adding empty IntervalTreeTest.cpp.

---
+++ IntervalTreeTest.cpp
@@ -0,0 +1,21 @@
+//
+//  main.cpp
+//  interval_tree_test2
+//
+//  Created by Jan on 08.11.12.
+//  Copyright (c) 2012 Jan. All rights reserved.
+//
+
+#include <iostream>
+
+#include "IntervalTree.hpp"
+
+int main(int argc, const char * argv[])
+{
+
+	// insert code here...
+	std::cout << "Nothing here, yet!\n";
+	
+	return 0;
+}
+
